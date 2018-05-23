0183621090cfae12b3874a323ad4f6799c85a5f5
BiG-MonK <s-monk-s@mail.ru>
2018-05-23 17:21:59 +0700

    Тест на сортировку файлов по расширению

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+var fs = require('fs')
+var path = require('path')
+ 
+var folder = process.argv[2]
+var ext = '.' + process.argv[3]
+ 
+fs.readdir(folder, function (err, files) {
+  if (err) return console.error(err)
+  files.forEach(function(file) {
+      if (path.extname(file) === ext) {
+          console.log(file)
+      }
+  })
+})
