c123de7df63167674d1df0ab0e1d8c4315b32cdf
Sukharev Sergey <s-monk-s@mail.ru>
2018-06-01 03:44:42 +0700

    Исправление ошибки запоминания тегов в буфере - innerHTML

--- Script_buffer.js
+++ Script_buffer.js
@@ -26,22 +26,22 @@ Top_window();
 var id = setInterval(function () {
     buffer_obj = gui.Clipboard.get();
     buffer_text = buffer_obj.get();
-    if (!buffer_array.some(elem => elem == buffer_text)) {  //--------- Сравнение вновь поступившего элемента в массив с уже имеющимися
+    if (!buffer_array.some(elem => elem === buffer_text)) {  //--------- Сравнение вновь поступившего элемента в массив с уже имеющимися
         var data_time = document.querySelector('.data-time');
         var newTr = document.createElement('tr');
         var newTd = document.createElement('td');
         var newTd2 = document.createElement('td');
         data_time.appendChild(newTr);                       //--------- Создание элемента Tr в элементе Tbody
         newTr.appendChild(newTd);                           //--------- Создание элемента Td в элементе Tr
-        newTd.innerHTML = Time();                           //--------- Заполнение ячейки времени
+        newTd.textContent = Time();                           //--------- Заполнение ячейки времени
         newTr.appendChild(newTd2);                          //--------- Создание второго элемента Td в том же элементе Tr
-        newTd2.innerHTML = buffer_text;                     //--------- Заполнение ячейки буфера
+        newTd2.textContent = buffer_text;                     //--------- Заполнение ячейки буфера
         buffer_array.push(buffer_text);                     //--------- Увеличение массива буфера еще одним значением
         var buffer_elem = document.querySelectorAll('.data-time tr td:last-child');
         for (let i = 0; i < buffer_elem.length; i++) {      //--------- Навешивание событий на каждый элемент Td тот что содержит значение буфера
             buffer_elem[i].onclick = function () {
-                buffer_obj.set(window.event.target.innerHTML, 'text');
-                //console.log(window.event.target.innerHTML);
+                buffer_obj.set(window.event.target.textContent, 'text');
+                //console.log(window.event.target.textContent);
                 return false;
             };//конец обработчика событий
         }//конец цыкла навешивания обработчика событий
