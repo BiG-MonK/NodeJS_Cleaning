97e570303994caddb8313a12561cdb5817c76f2e
Sukharev Sergey <s-monk-s@mail.ru>
2018-06-13 03:00:25 +0700

    Рекурсивное удаление файлов и папок в отдельные функции РАБОТАЕТ

--- Script_clear.js
+++ Script_clear.js
@@ -1,9 +1,46 @@
 const fs = require('fs');
+var arr_files = [];
+var arr_dir = [];
+var path = './test';
+var size_all_files = 0;
 
-getSize('./test', function(err, size) {
-  if (err) { throw err; }
-
-  console.log(size + ' bytes');
-  console.log((size / 1024 / 1024).toFixed(2) + ' Mb');
-});
- 
+var time = performance.now();                         //--- Засекает время выполнения программы
+//------------------------------------------------------------------------------------------------------------------------------------------
+list_dir = function (path_dir) {//--- Рекуривная функция просмотра папки на наличие файлов
+  var cur_path = '';
+  var content_dir = fs.readdirSync(path_dir);         //--- Дает список содержимого папки
+  for (let i = 0; i < content_dir.length; i++) {      //--- Прогон списка через цикл
+    cur_path = path_dir + '/' + content_dir[i];       //--- Относительный путь прогоняемого элемента
+    if (fs.statSync(cur_path).isFile()) {             //--- Проверка на файл\папка
+      arr_files.push(path_dir + '/' + content_dir[i]);//--- Выписываем в отдельный массив файлов полный путь нового файла
+      size_all_files += fs.statSync(cur_path).size;   //--- Подсчет общего веса файлов всех
+    } else {
+      arr_dir.push(path_dir + '/' + content_dir[i]);  //--- Выписываем в отдельный массив папок полный путь папки
+      list_dir(cur_path);                             //--- Если папка то рекурсивный вызов
+    }
+  };
+};//--- Конец рекурсивной функции просмотра папки
+//------------------------------------------------------------------------------------------------------------------------------------------
+delete_dir = function () {//--- Функция удаления файлов и папок
+  for (let i = 0; i < arr_files.length; i++) {        //--- Прогон списка через цикл
+    fs.unlinkSync(arr_files[i]);                      //--- Удаляем поочередно все файлы, пути которых находятся в общем массиве файлов
+  };
+  do {
+    for (let i = 0; i < arr_dir.length; i++) {
+      var content_dir = fs.readdirSync(arr_dir[i]);
+      if (content_dir == '') {                        //--- Если контента в папке нет то..
+        fs.rmdirSync(arr_dir[i]);                     //--- Удаляем пустую папку
+        arr_dir.splice([i], 1);                       //--- Вырезаем из массива папок вместе с ключем массива пустую папку
+      }
+    }
+  }
+  while (arr_dir.length != 0);                        //--- Пока не закончится массив папок продолжаем перебирать и удалять пустые папки
+};//--- Конец функции удаления файлов и папок
+//------------------------------------------------------------------------------------------------------------------------------------------
+list_dir(path);
+console.log('Всего папок в этой папке: ' + arr_dir.length);
+console.log('Всего файлов в этой папке: ' + arr_files.length);
+console.log('Размер всех файлов равен: ' + (size_all_files / 1024 / 1024).toFixed(4) + ' Mb');
+delete_dir();
+time = performance.now() - time;
+console.log('Время выполнения программы = ', time, 'msec');
